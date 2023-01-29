d05d797219b6e38a83ad2dcece6ba9a9d5a6267d
Lam <vtlam.caha@gmail.com>
2023-01-29 22:37:34 +0700

    a git

---
+++ models/cart.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    // Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+
+      // Analyze the cart => Find existing product
+      const idsp = cart.products.findIndex((prod) => prod.id === id);
+      const sp = cart.products[idsp];
+      let updatedProduct;
+      // Add new product/ increase quantity
+      if (sp) {
+        updatedProduct = { ...sp };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[idsp] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};

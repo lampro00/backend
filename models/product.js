d496a7d2731ea8340640b8d09def05fa312a25db
Lam <vtlam.caha@gmail.com>
2023-01-30 22:51:44 +0700

    lab9.1

---
+++ models/product.js
@@ -0,0 +1,22 @@
+const db = require("../util/database");
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {}
+
+  static deleteById(id) {}
+
+  static fetchAll(cb) {
+    return db.execute("select * from products");
+  }
+
+  static findById(id) {
+    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
+  }
+};
